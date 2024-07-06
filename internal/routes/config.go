321ed6b5ca3ada763bcb3b377e210bd6edc0396a
Shridhar2104 <155125695+Shridhar2104@users.noreply.github.com>
2024-07-06 11:00:37 +0530

    folder struct

---
+++ internal/routes/config.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"net/http"
+	"time"
+	"github.com/go-chi/cors"
+)
+
+
+type Config struct{
+	timeout time.Duration
+}
+func NewConfig() *Config{
+	return &Config{}
+}
+
+func (c *Config)Cors(next http.Handler) http.Handler{
+
+	return cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		MaxAge: 5,
+		AllowedCredentials: true,
+		ExposedHeaders: []string{"*"},	
+	}).Handler(next)
+
+}
+
+func (c *Config)SetTimeOut(timeInSeconds int) *Config{
+	c.timeout = time.Duration(timeInSeconds) * time.Second
+	return c
+}
+
+func (c *Config)GetTimeOut() time.Duration{
+	return c.timeout
+}
